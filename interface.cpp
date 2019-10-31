a11ac7d1c2dd59e1c83d478a37a4cb8ab9627584
meduardcn <m.eduardacn@gmail.com>
2019-10-31 20:23:33 -0300

    handle input implemented

--- interface.cpp
+++ interface.cpp
@@ -17,40 +17,62 @@ void handle(string input){
     string token = input.substr(0, pos);
     input.erase(0, pos + delimiter.length());
 
-    cout << token << endl;
     if (token == "ep"){
         // ep endereço do final do programa
         pos = input.find(delimiter);
         token = input.substr(0, pos);
         int value = stoi(token);
         memory.resize(value);
-        cout << "memory size";
-        cout << value << endl;
+
+        // tuple<string, int, int, int> tp("ep", value,0,0);
+        // memory[value] = tp
+        // cout << get<0>(tp)  << endl;
+        // cout << memory.size() << endl;
     }
     else if (token == "ji"){
         // ji define um saldo incondicional
-        
+        pos = input.find(delimiter);
+        token = input.substr(0, pos);
+        input.erase(0, pos + delimiter.length());
+        int adr = stoi(token);
+        pos = input.find(delimiter);
+        token = input.substr(0, pos);
+        int tgt = stoi(token);
+        tuple<string, int, int, int> tp("ji", adr, tgt, NULL);
+        memory[adr] = tp;
     }
     else if (token == "bi"){
         // bi define um salto condicional.
+        pos = input.find(delimiter);
+        token = input.substr(0, pos);
+        input.erase(0, pos + delimiter.length());
+        int adr = stoi(token);
+        pos = input.find(delimiter);
+        token = input.substr(0, pos);
+        input.erase(0, pos + delimiter.length());
+        int tgt = stoi(token);
+        pos = input.find(delimiter);
+        token = input.substr(0, pos);
+        int pbd = stoi(token);
+        tuple<string, int, int, int> tp("bi", adr, tgt, pbd);
+        memory[adr] = tp;
     }
-    // while ((pos = input.find(delimiter)) != string::npos)
-    // {
-    //     token = input.substr(0, pos);
-    //     cout << token << std::endl;
-    //     input.erase(0, pos + delimiter.length());
-    // }
 }
 
 void readInput(){
     string input;
-    while (getline(cin, input) ){
-        cout << input << endl;
+    while(getline(cin, input)){
         handle(input);
     }
 }
 
+void printVector(){
+    for (int i = 0; i < memory.size(); i++){
+        cout << get<0>(memory[i]) << " "<< get<1>(memory[i]) << " " << get<2>(memory[i]) << " " << get<3>(memory[i])  << endl;
+    }
+}
 int main(){
     readInput();
+    // printVector();
     return 0;
 }
