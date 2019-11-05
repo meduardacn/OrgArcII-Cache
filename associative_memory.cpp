699fe55432c53925a22e5bee8a8bce52a352d20e
Gabriel <gabriel.justo@acad.pucrs.br>
2019-11-05 13:02:28 -0300

    associative cache program started

---
+++ associative_memory.cpp
@@ -0,0 +1,19 @@
+//Compile: g++ associative_memory.cpp -o associative_memory
+//Run: ./associative_memory <cache size> <words per blocks> <word size> <pathways>
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char argv[])
+{
+    int cache_size = argv[1];
+    int words_per_block = argv[2];
+    int word_size = argv[3];
+    int pathways = argv[4];
+    
+    
+    return 0;
+}
