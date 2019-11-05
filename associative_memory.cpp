56bfbe45dea9e2a5e640b3936c87c444bcce5c7f
Gabriel <gabriel.justo@acad.pucrs.br>
2019-11-05 15:41:59 -0300

    just a little bit changes

--- associative_memory.cpp
+++ associative_memory.cpp
@@ -7,6 +7,18 @@
 
 using namespace std;
 
+
+
+struct line 
+{
+    unsigned char valid_bit;
+    unsigned char* tag;
+    unsigned char* data;
+
+}Line;
+
+
+
 int main(int argc, char argv[])
 {
     int cache_size = argv[1];
@@ -14,6 +26,7 @@ int main(int argc, char argv[])
     int word_size = argv[3];
     int pathways = argv[4];
     
+    int n_linhas = cache_size/(words_per_block * word_size);
     
     return 0;
 }
